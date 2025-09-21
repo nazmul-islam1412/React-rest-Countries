95ac682ac7a36c7d914c521c2dfc57e1b46662ae
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-09-21 22:37:14 +0600

    added full

--- src/assets/Country/country.jsx
+++ src/assets/Country/country.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css';
 
 const Country = ({country}) => {
+
+    const [visited,setvisited]=useState(false);
+
     
 
 const handleVisited = () =>{
-    console.log("Clicked")
+    
+    // if(visited){
+    //     setvisited(false)
+    // }
+    // else{
+    //     setvisited(true)
+    // }
+    setvisited(visited ? false : true)
 }
     return (
-        <div className='Country'>
+        <div className={`Country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt="" />
             <h3>Population:{country.population.population}</h3>
             <h3>Name: {country.name.common}</h3>
             <h3>Area: {country.area.area} 
                 {country.area.area > 300000 ? " Big Country" : " Small Country"}
             </h3>
-            <button onClick={handleVisited}>Not visted</button>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Not visited"}</button>
             
         </div>
     );
